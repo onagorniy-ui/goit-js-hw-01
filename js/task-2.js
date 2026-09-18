61e03e9d46f26bf6c071fa2f4fbd5243cf3a0c8f
Oleg <o.nagorniy@gmail.com>
2026-09-18 14:22:06 +0300

    Done 3 tasks from HW1 JS

--- js/task-2.js
+++ js/task-2.js
@@ -1,95 +1,12 @@
-/*
-TODO: Вивести привітання
-TODO: Є змінні firstName та age.
-TODO: Склади шаблонний рядок: "Привіт, мене звати firstName і мені age років."
-*/
+'use strict';
 
-const firstName = 'Oleg';
-const age = 25;
-
-const message = `Привіт, мене звати ${firstName} і мені ${age} років.`;
-
-console.log(message);
-
-const firstName2 = 'Steve';
-const age2 = 30;
-
-const message2 = 'Привіт, мене звати ' + firstName2 + ' і мені ' + age2 + ' років.';
-
-console.log(message2);
-
-/*
-TODO: Є змінні a та b. Виведи рядок: "Сума a + b = 13"
-*/
-
-const a = 15;
-const b = 20;
-
-const message3 = `Summ a+b = ${a + b}`;
-console.log(message3);
-
-/*
-TODO: Створи функцію getEventInfo(event, date, location),
-TODO: яка буде повертати рядок з інформацією про подію: "Подія [event] відбудеться [date]  у місті [location]"
-*/
-function getEventInfo(event, date, location) {
-  return `Event ${event} will happen ${date} in ${location}`;
+//Declare function
+function getShippingMessage(country, price, deliveryFee) {
+  const totalPrice = price + deliveryFee;
+  return `Shipping to ${country} will cost ${totalPrice} credits`;
 }
-console.log(getEventInfo('Hackaton', '15 June', 'Kyiv'));
 
-/*
-TODO: Створи функцію formatUser(name, email), яка повертає рядок: "Користувач [Ім'я] має пошту: [email]"
-*/
-function formatUser(name, email) {
-  return `User ${name} has email: ${email}`;
-}
-console.log(formatUser('Mollie', 'test1@gmail.com'));
-
-/*
-TODO: Перетвори рядок на число.
-*/
-const str = '10';
-const num = +str;
-console.log(num);
-
-/*
-TODO: Є зміна weight, в якій зберігається значення ваги людини.
-TODO: Отримай числове значення ваги.
-*/
-
-const weight = '85kg';
-const nemWeight = Number.parseInt(weight);
-
-console.log(nemWeight);
-
-/*
-TODO: Є зміна height, в якій зберігається значення висоти людини.
-TODO: Отримай числове значення висоти.
-*/
-
-const height = '175.4cm';
-const nemHieght = Number.parseFloat(height);
-
-console.log(height);
-console.log(nemHieght);
-
-/*
-TODO: Створи функцію getCubeVolume(side), яка обчислює обʼєм куба за переданною стороною.
-TODO: Обʼєм куба розраховується за формулою V = a^3.
-*/
-
-function getCubeVolume(side) {
-  const numSide = Number.parseFloat(side);
-  return Math.pow(numSide, 3);
-}
-console.log(getCubeVolume('5cm'));
-console.log(getCubeVolume('3.3cm'));
-
-// function getCubeVolume(side) {
-//   const numericalSide = Number.parseInt(side);
-
-//   return numericalSide ** 3;
-// }
-
-// console.log(getCubeVolume('5cm'));
-// console.log(getCubeVolume('3cm'));
+//Do tests
+console.log(getShippingMessage('Australia', 120, 50));
+console.log(getShippingMessage('Germany', 80, 20));
+console.log(getShippingMessage('Sweden', 100, 20));
--- js/task-3.js
+++ js/task-3.js
@@ -1,95 +1,13 @@
-/*
-TODO: Вивести привітання
-TODO: Є змінні firstName та age.
-TODO: Склади шаблонний рядок: "Привіт, мене звати firstName і мені age років."
-*/
+'use strict';
 
-const firstName = 'Oleg';
-const age = 25;
-
-const message = `Привіт, мене звати ${firstName} і мені ${age} років.`;
-
-console.log(message);
-
-const firstName2 = 'Steve';
-const age2 = 30;
-
-const message2 = 'Привіт, мене звати ' + firstName2 + ' і мені ' + age2 + ' років.';
-
-console.log(message2);
-
-/*
-TODO: Є змінні a та b. Виведи рядок: "Сума a + b = 13"
-*/
-
-const a = 15;
-const b = 20;
-
-const message3 = `Summ a+b = ${a + b}`;
-console.log(message3);
-
-/*
-TODO: Створи функцію getEventInfo(event, date, location),
-TODO: яка буде повертати рядок з інформацією про подію: "Подія [event] відбудеться [date]  у місті [location]"
-*/
-function getEventInfo(event, date, location) {
-  return `Event ${event} will happen ${date} in ${location}`;
+//Declare function
+function getElementWidth(content, padding, border) {
+  //Use parseFloat to convert the string to a number, then add them together
+  const total = Number.parseFloat(content) + Number.parseFloat(padding) * 2 + Number.parseFloat(border) * 2;
+  return total;
 }
-console.log(getEventInfo('Hackaton', '15 June', 'Kyiv'));
 
-/*
-TODO: Створи функцію formatUser(name, email), яка повертає рядок: "Користувач [Ім'я] має пошту: [email]"
-*/
-function formatUser(name, email) {
-  return `User ${name} has email: ${email}`;
-}
-console.log(formatUser('Mollie', 'test1@gmail.com'));
-
-/*
-TODO: Перетвори рядок на число.
-*/
-const str = '10';
-const num = +str;
-console.log(num);
-
-/*
-TODO: Є зміна weight, в якій зберігається значення ваги людини.
-TODO: Отримай числове значення ваги.
-*/
-
-const weight = '85kg';
-const nemWeight = Number.parseInt(weight);
-
-console.log(nemWeight);
-
-/*
-TODO: Є зміна height, в якій зберігається значення висоти людини.
-TODO: Отримай числове значення висоти.
-*/
-
-const height = '175.4cm';
-const nemHieght = Number.parseFloat(height);
-
-console.log(height);
-console.log(nemHieght);
-
-/*
-TODO: Створи функцію getCubeVolume(side), яка обчислює обʼєм куба за переданною стороною.
-TODO: Обʼєм куба розраховується за формулою V = a^3.
-*/
-
-function getCubeVolume(side) {
-  const numSide = Number.parseFloat(side);
-  return Math.pow(numSide, 3);
-}
-console.log(getCubeVolume('5cm'));
-console.log(getCubeVolume('3.3cm'));
-
-// function getCubeVolume(side) {
-//   const numericalSide = Number.parseInt(side);
-
-//   return numericalSide ** 3;
-// }
-
-// console.log(getCubeVolume('5cm'));
-// console.log(getCubeVolume('3cm'));
+//Do tests
+console.log(getElementWidth('50px', '8px', '4px'));
+console.log(getElementWidth('60px', '12px', '8.5px'));
+console.log(getElementWidth('200px', '0px', '0px'));
